fd2493f7d04161f8d3aefebba4252a8ca5912d57
Honey410 <honayjhamnani410@gmail.com>
2021-07-08 14:10:38 +0530

    Modify

--- code.rb
+++ code.rb
@@ -20,19 +20,63 @@
 #  a = Vehicle.vehicle_in()
 #  end
 
-class Parking_Lot_Problem
-    puts "How many slots do you want?"   
-def initialize(slt)
-    @slots = []
-    slt.to_i
-      slot_number = index + 1
-      slots[index] = Slot.new(slot_number)
-    end
-    puts "Parking Slot =  #{slt} "
+puts "Which Operation u want?"
+puts "1 ADD Car"
+puts "2 Remove Car"
+puts "3 Status"
+puts "4 Search Car With color"
+puts "5 Search Car With Num"
+puts "6 Park car in alloted slot"
+puts "7 Entry time:"
+puts "8 Exit time"
+puts "9 Exit"
 
-    def Vehicle_in(vehicle_number, vehicle_color)
-        if self.vehicle
-          puts 'Occcupied'
-        else
-          self.vehicle=Vehicle.new(vehicle_number, vehicle_color)
-        end
+Operation = gets.chomp.to_i
+case Operation
+when 1
+  CreateSlot
+when 2
+  remove_slot
+when 3
+  ShowStatus
+when 4
+  SearchData
+when 5
+  SearchSlot
+when 7
+  EntryTime
+when 8
+  ExitTime
+else
+  break
+end
+
+puts "How many slots do you want?"  
+def CreateSlot()
+  puts "Create parking Slot : "
+  slots = gets.chomp.to_i
+  i = 0
+  while i < slots do
+    parking_hash = {}
+    puts "Park"
+    plate_num = gets.chomp
+    color = gets.chomp
+    parking_hash['id'] = id+=1
+    parking_hash['plate_num'] = plate_num
+    parking_hash['color'] = color
+    parking_hash['alloted'] = true
+    i+=1
+  end
+end
+def remove_slot
+  
+end 
+def ExitTime
+  puts "Slot want to Empty?"
+  remove_id = gets.chomp.to_i
+  if slots : length<remove_id
+    puts "Invalid Slot"
+      return 0
+  end
+end    
+     
